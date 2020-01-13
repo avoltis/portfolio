23447b9357a472881af6487623a3220d1aab707a
avoltis <v.agolli@live.com>
2020-01-13 17:01:52 +0100

    Updates

---
+++ precache-manifest.ec676a75ac888cff2de77c5928d1ef71.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0065cdf805d629a58d1226a6561d73bf",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "60cf22a90a91667f8170",
+    "url": "/portfolio/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "257b328f451351ff8be9",
+    "url": "/portfolio/static/js/2.fdc2326f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/portfolio/static/js/2.fdc2326f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "60cf22a90a91667f8170",
+    "url": "/portfolio/static/js/main.94eb0834.chunk.js"
+  },
+  {
+    "revision": "6fef3673099b4dcdb973",
+    "url": "/portfolio/static/js/runtime-main.fed7eca6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
