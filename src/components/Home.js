bbe697bab55eccf781e2a6b1b95bcb2ff0a82b09
avoltis <v.agolli@live.com>
2020-01-15 11:20:55 +0100

    import bulma

--- src/components/Home.js
+++ src/components/Home.js
@@ -28,20 +28,14 @@ class Home extends Component {
     console.log(portfolioQuery.getPortfolio);
 
     return (
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="section">
+        <div className="container">
+          <h1 className="title">Hello World</h1>
+          <p className="subtitle">
+            My first website with <strong>Bulma</strong>!
+          </p>
+        </div>
+      </section>
     );
   }
 }
